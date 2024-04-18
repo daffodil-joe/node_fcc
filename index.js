cde5834ab0ca3afde47b926ddb227fbc315e2db8
Joe Kane <joe.kane@daffodil-software.co.uk>
2024-04-18 16:55:50 +0100

    not working, mogno connection underway

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 
-app.listen(3000,()=> {
-    console.log('Server is running on port 3000');
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
 
-app.get('/', (req, res) => {
-    res.send("Hello from Node b");
-})
+app.get("/", (req, res) => {
+  res.send("Hello from your node container!");
+});
+
+mongoose
+  .connect("mongodb://root:password@mongo:27017/test")
+  .then(() => console.log("Connected!"));
