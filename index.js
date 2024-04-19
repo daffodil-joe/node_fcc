9283a7f95ed8955ff7409b956eb65632b259a3d3
Joe Kane <joe.kane@daffodil-software.co.uk>
2024-04-19 13:57:45 +0100

    db connected and persisted

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
 const app = express();
 
+mongoose
+  .connect("mongodb://root:password@mongo:27017/")
+  .then(() => {
+    console.log("**DB**: Connected to test database!");
+  })
+  .catch(() => {
+    console.log("**DB**: Database connection error");
+  });
+
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("**SERVER**: Server is running on port 3000");
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello from your node container!");
+  res.send("Hello from your docker container!");
 });
-
-mongoose
-  .connect("mongodb://root:password@mongo:27017/test")
-  .then(() => console.log("Connected!"));
